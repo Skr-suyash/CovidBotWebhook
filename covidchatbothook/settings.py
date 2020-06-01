ca127b19b1b90933c37bd5874fd35952538f1c53
= <=>
2020-06-01 11:58:21 +0530

    changed settings for deploy

--- covidchatbothook/settings.py
+++ covidchatbothook/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3-j$j#g&*a=@pzxc1sy70r-=49tn+8zg^(-aeob@hh@y8u4*@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = []
 
@@ -116,6 +116,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
